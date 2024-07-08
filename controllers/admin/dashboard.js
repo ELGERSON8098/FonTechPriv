7c8a56d815fa054f214da1ad93ce548e49951147
Allan-fuentes <108203747+Allan-fuentes@users.noreply.github.com>
2024-07-08 11:24:13 -0600

    borrando lo que no utilizamos

--- controllers/admin/dashboard.js
+++ controllers/admin/dashboard.js
@@ -30,28 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
 *   Parámetros: ninguno.
 *   Retorno: ninguno.
 */
-const graficoBarrasCategorias = async () => {
-    // Petición para obtener los datos del gráfico.
-    const DATA = await fetchData(PRODUCTO_API, 'cantidadProductosCategoria');
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
-    if (DATA.status) {
-        // Se declaran los arreglos para guardar los datos a graficar.
-        let categorias = [];
-        let cantidades = [];
-        // Se recorre el conjunto de registros fila por fila a través del objeto row.
-        DATA.dataset.forEach(row => {
-            // Se agregan los datos a los arreglos.
-            categorias.push(row.nombre_categoria);
-            cantidades.push(row.cantidad);
-        });
-        // Llamada a la función para generar y mostrar un gráfico de barras. Se encuentra en el archivo components.js
-        barGraph('chart1', categorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
-    } else {
-        document.getElementById('chart1').remove();
-        console.log(DATA.error);
-    }
-}
-
 /*
 *   Función asíncrona para mostrar un gráfico de pastel con el porcentaje de productos por categoría.
 *   Parámetros: ninguno.
