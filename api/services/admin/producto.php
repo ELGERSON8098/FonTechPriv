3a5107f401766fd57496962775829fcc9bf2304d
Allan-fuentes <20220406@ricaldone.edu.sv>
2024-07-16 07:52:31 -0600

    Producto en php

--- api/services/admin/producto.php
+++ api/services/admin/producto.php
@@ -140,6 +140,20 @@ if (isset($_GET['action'])) {
                     $result['error'] = 'Ocurrió un problema al eliminar el producto';
                 }
                 break;
+                case 'cantidadProductosCategoria':
+                    if ($result['dataset'] = $producto->cantidadProductosCategoria()) {
+                        $result['status'] = 1;
+                    } else {
+                        $result['error'] = 'No hay datos disponibles';
+                    }
+                    break;
+                case 'porcentajeProductosCategoria':
+                    if ($result['dataset'] = $producto->porcentajeProductosCategoria()) {
+                        $result['status'] = 1;
+                    } else {
+                        $result['error'] = 'No hay datos disponibles';
+                    }
+                    break;
             default:
                 $result['error'] = 'Acción no disponible fuera de la sesión';
         }
